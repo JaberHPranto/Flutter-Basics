11daf144cd68f93bd3e7f3d3877c31a079d4994c
JaberHPranto <jhpranto53@gmail.com>
2021-07-23 00:39:12 +0600

    first app

---
+++ lib/homePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String language = 'flutter';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inventory Manager'),
+      ),
+      body: Center(child: Container(child: Text('I am learning $language'))),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/homePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-      home: Material(
-        child: Center(child: Container(child: Text('I am learning flutter'))),
-      )
+        home: HomePage(),
       );
   }
 }
